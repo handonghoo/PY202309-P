4d27f1144f43b4233305802d27120050fbe509a7
handonghoo <handonghoo0812@gmail.com>
2023-11-20 19:45:54 +0900

    사용할 모듈 탐색 및 금주 목표 구현  함수 설정

---
+++ Sources/main.py
@@ -0,0 +1,25 @@
+#필요할법한 모듈들을 긁어 모아봤습니다. 
+import cv2 as cv
+from difflib import get_close_matches  
+import subprocess 
+import os
+import time
+import pyautogui  
+import pyperclip 
+
+
+
+#카카오톡을 키는 것을 자동화 하는 함수
+def kill_kakao():
+    pass
+
+
+#카카오톡을 키는 것을 자동화 하는 함수
+def kill_kakao():
+    pass
+
+#카카오톡 로그인을 자동화하는 함수
+def login_kakao():
+    pass
+
+#일단 요번주 목표 = 켜고 끄고 로그인하는것까지 구현해보기!
